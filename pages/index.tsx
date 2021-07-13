31f159f0ddd2b96367f810e33b3b4d4cdce89469
Jonghyeon Ko <flowithusdev@gmail.com>
2021-07-13 20:00:48 +0900

    Update - Home getStaticProps

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,9 @@ import Head from 'next/head';
 import Alert from '../components/alert';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
+import { getSortedPostsData } from '../lib/posts';
 
-export default function Home() {
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Alert type="error">하이</Alert>
@@ -16,7 +17,25 @@ export default function Home() {
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
+        {allPostsData.map(({ id, date, title }) => (
+          <li key={id}>
+            {title}
+            <br />
+            {id}
+            <br />
+            {date}
+          </li>
+        ))}
       </section>
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
 }
